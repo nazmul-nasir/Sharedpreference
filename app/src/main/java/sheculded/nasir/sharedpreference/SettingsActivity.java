6d377aefbead805247dc4f4f19bc4abae74b19cb
nazmul-nasir <nazmul.nasir@gmail.com>
2016-01-25 02:39:52 +0600

    Initial commit

---
+++ app/src/main/java/sheculded/nasir/sharedpreference/SettingsActivity.java
@@ -0,0 +1,19 @@
+package sheculded.nasir.sharedpreference;
+
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Define the xml file used for preferences
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
